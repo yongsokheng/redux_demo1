37eed01a82ebddee3e917cdc355a98814c8e9ab5
yongsokheng <yongsokheng@ymail.com>
2017-07-29 22:43:21 +0700

    Redux thunk

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,11 +1,15 @@
 const INIT_STATE = {
-  text: ""
+  text: "",
+  data: [],
+  loading: true
 };
 
 export default ListData = (state = INIT_STATE, action) => {
   console.log(action);
   if(action.type == "text_input") {
     return {...state, text: action.payload};
+  } else if(action.type == "fetch_data") {
+    return {...state, data: action.payload, loading: false}
   } else {
     return state;
   }
